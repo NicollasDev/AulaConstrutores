b945790f3a2812651f11feb00e44454d545785b6
NicollasDileo06 <nicgato2013@gmail.com>
2020-07-09 00:03:07 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula16Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          //Instancia sem argumentos  
+         Produto sapato = new Produto(23);
+         sapato.Nome = "Vizzano";
+        //Instancia com 1 argumento
+         Produto chinelo = new Produto(45);
+         System.Console.WriteLine(chinelo.Codigo);
+         
+         //Instancia com todos os atributos como argumento
+         Produto camisa = new Produto(34, "Supreme", "Azul", 234);
+         System.Console.WriteLine(camisa.Codigo);
+         System.Console.WriteLine(camisa.Nome);
+         System.Console.WriteLine(camisa.Descricao);
+         System.Console.WriteLine(camisa.Estoque);
+        }
+    }
+}
